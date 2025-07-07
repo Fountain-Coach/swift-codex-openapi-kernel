28fb78cecfea374da602815a12ba799127096e05
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-07-07 07:02:14 +0200

    Add AGENTS instructions

--- Sources/Parser/OpenAPISpec.swift
+++ Sources/Parser/OpenAPISpec.swift
@@ -1,5 +1,30 @@
 import Foundation
 
 public struct OpenAPISpec: Codable {
+    public struct Components: Codable {
+        public var schemas: [String: Schema]
+    }
+
+    public struct Schema: Codable {
+        public struct Property: Codable {
+            public var type: String
+        }
+
+        public var type: String
+        public var properties: [String: Property]?
+    }
+
     public let title: String
+    public var components: Components?
+}
+
+extension OpenAPISpec.Schema.Property {
+    public var swiftType: String {
+        switch type {
+        case "string": return "String"
+        case "integer": return "Int"
+        case "boolean": return "Bool"
+        default: return "String"
+        }
+    }
 }
